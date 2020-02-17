50221c4ad5f9c464f45c5ab4b9a8464f3b3f7fc9
sushant1696 <shaan160196@gmail.com>
2020-02-17 10:36:35 +0530

    [sushant] Add . added the usecase4

--- SnakeLadderGame.sh
+++ SnakeLadderGame.sh
@@ -1,1 +1,27 @@
-Welcome to ladder snake game 
+PlayerPosition=0
+DiceRoll(){
+ DiceOutput=$(($(($RANDOM%6))+1))
+return $DiceOutput
+}
+DiceRoll
+echo "dice output is $DiceOutput
+
+echo 1 for No play
+echo 2 for ladder
+echo 3 for snak
+echo "you choose option $option"
+
+option=$(($(($RANDOM%3))+1))
+case $option in
+
+1) echo $PlayerPosition
+   ;;
+
+2) PlayerPosition = $(( $PlayerPosition ))
+  echo " player position :$PlayerPosition"
+   ;;
+
+3) PlayerPosition = $(( $PlayerPosition ))
+   echo player position is $PlayerPosition
+   ;;
+esac
